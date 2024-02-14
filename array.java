965b4722047e3354b1cdbc8f2965a3b378113b71
Shahar-bin-Akbar <125555896+Shahar-bin-Akbar@users.noreply.github.com>
2024-02-14 18:10:50 +0530

    Aligned array.java

--- array.java
+++ array.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class array{
-    public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
-      int[] arr = new int[5];
-      for(int i= 0;i<arr.length;i++){
-        arr[i] = in.nextInt();
-      }
-      System.out.println(Arrays.toString(arr));
+public class array {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int[] arr = new int[5];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = in.nextInt();
     }
+    System.out.println(Arrays.toString(arr));
+  }
 }
